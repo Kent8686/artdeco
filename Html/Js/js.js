cae3d8b11b556728f077ad9ebb06d7bd3114624c
Kent8686 <kennethxicay88@gmail.com>
2025-12-24 13:34:48 -0600

    Update js.js

--- Html/Js/js.js
+++ Html/Js/js.js
@@ -10,3 +10,17 @@ cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
 
+const imagenes = document.querySelectorAll('.lazy');
+
+const observador = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src; // Aquí JS carga la imagen real
+      img.classList.remove('lazy');
+      observador.unobserve(img); // Ya la cargó, deja de vigilarla
+    }
+  });
+});
+
+imagenes.forEach(img => observador.observe(img));
